bd57a4e799da13977fb907a763f1ca2a4a90d72f
suker799 <68619506@qq.com>
2025-09-29 17:38:01 +0800

    Update __init__.py

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -3,7 +3,7 @@ from .DOTA1_5 import DOTA1_5Dataset, DOTA1_5Dataset_v3, DOTA1_5Dataset_v2
 from .DOTA2 import DOTA2Dataset
 from .DOTA2 import DOTA2Dataset_v2
 from .DOTA2 import DOTA2Dataset_v3, DOTA2Dataset_v4
-from .HRSC import HRSCL1Dataset
+from .HRSC import HRSCL1Dataset, HRSCL2Dataset
 from .coco import CocoDataset
 from .concat_dataset import ConcatDataset
 from .custom import CustomDataset
@@ -19,6 +19,6 @@ __all__ = [
     'DOTA2Dataset_v2', 'DOTA2Dataset_v3', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
-    'ExtraAugmentation', 'HRSCL1Dataset', 'DOTADataset_v3',
+    'ExtraAugmentation', 'HRSCL1Dataset', 'HRSCL2Dataset', 'DOTADataset_v3',
     'DOTA1_5Dataset', 'DOTA1_5Dataset_v3', 'DOTA1_5Dataset_v2', 'DOTA2Dataset_v4'
 ]
